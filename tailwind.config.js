a4e9fed911344a7e52e0423646d8ebde65e16caf
nison79 <georgenison@gmail.com>
2020-11-10 23:45:47 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#FF6363",
+        secondary: {
+          100: '#E2E2D5',
+          200: '#88883',
+        }
+      },
+      fontFamily: {
+        body:['Poppins']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
